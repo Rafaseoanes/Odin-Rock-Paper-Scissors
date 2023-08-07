121ea6c5f79fdd7139c995dee2ed0d2ea753430b
Rafaseoanes <rafaseoanes9@gmail.com>
2023-08-06 19:54:08 -0500

    getPlayerChoice updated whit proper user validation

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // getComputerChoice is a function that randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
-let getComputerChoise = function () {
+let getComputerChoice = function () {
   let availableOptions = ["rock", "paper", "scissors"];
   let index = Math.floor(Math.random() * 3);
 
@@ -7,44 +7,48 @@ let getComputerChoise = function () {
 };
 
 // getPlayerChoice is a function that prompt the player to choose between ‘Rock’, ‘Paper’ or ‘Scissors’.
-let getPlayerChoise = function () {
-  let playerChoise = prompt(
-    "Choose from 'rock', 'paper' or 'scissors'"
-  ).toLocaleLowerCase();
-  return playerChoise;
+let getPlayerChoice = function () {
+  let playerChoice;
+
+  while (
+    !(
+      playerChoice === "rock" ||
+      playerChoice === "paper" ||
+      playerChoice === "scissors"
+    )
+  ) {
+    playerChoice = prompt(
+      "Choose from 'rock', 'paper' or 'scissors'"
+    ).toLowerCase();
+  }
+  return playerChoice;
 };
 
 let computerScore = 0,
   playerScore = 0;
 
-// playRound is a function that takes the two player choises and defines who wins’.
+// playRound is a function that takes the two player choices and defines who wins’.
 let playRound = function (playerSelection, computerSelection) {
-  if (
-    playerSelection === "rock" ||
-    playerSelection === "paper" ||
-    playerSelection === "scissors"
+  if (playerSelection === computerSelection) {
+    return `You chose ${playerSelection} and the computer chose ${computerSelection}, is a tie!`;
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
   ) {
-    if (playerSelection === computerSelection) {
-      return `You chose ${playerSelection} and the computer chose ${computerSelection}, is a tie!`;
-    } else if (
-      (playerSelection === "rock" && computerSelection === "scissors") ||
-      (playerSelection === "paper" && computerSelection === "rock") ||
-      (playerSelection === "scissors" && computerSelection === "paper")
-    ) {
-      playerScore += 1;
-      return `You chose ${playerSelection} and the computer chose ${computerSelection}, you win!`;
-    } else {
-      computerScore += 1;
-      return `You chose ${playerSelection} and the computer chose ${computerSelection}, you lose!`;
-    }
+    playerScore += 1;
+    return `You chose ${playerSelection} and the computer chose ${computerSelection}, you win!`;
   } else {
-    alert("Please enter a valid option: 'rock', 'paper' or 'scissors'");
+    computerScore += 1;
+    return `You chose ${playerSelection} and the computer chose ${computerSelection}, you lose!`;
   }
 };
 
 //getWinner is a function that keeps defines who wins base on playerScore and computerScore
 function getWinner() {
-  if (playerScore > computerScore) {
+  if (playerScore === computerScore) {
+    return `You have ${playerScore} points and the computer have ${computerScore} points. it's a tie!`;
+  } else if (playerScore > computerScore) {
     return `You have ${playerScore} points and the computer have ${computerScore} points. You win!`;
   } else {
     return `You have ${playerScore} points and the computer have ${computerScore} points. You lose!`;
@@ -53,10 +57,9 @@ function getWinner() {
 
 function game() {
   for (let i = 0; i < 5; i++) {
-    console.log(playRound(getPlayerChoise(), getComputerChoise()));
+    console.log(playRound(getPlayerChoice(), getComputerChoice()));
   }
   console.log(getWinner());
 }
 
 game();
-//  console.log(playRound(getPlayerChoise(), getComputerChoise()))
